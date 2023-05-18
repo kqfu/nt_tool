662bc647b54e839b2bec2d19a39b07e4d0ba507b
Kevin Fu <qiangkfu@gmail.com>
2023-05-17 23:00:10 -0700

    Fix the script, Load limit and min_run_gap from event (#4)
    * Fix the script
    
    * Load limit and min_run_gap from event

--- cron/app.py
+++ cron/app.py
@@ -6,9 +6,6 @@ from aa_searcher import Aa_Searcher
 from ac_searcher import Ac_Searcher
 from dl_searcher import Dl_Searcher
 
-LIMIT = 30
-MIN_RUN_GAP = 4 * 3600
-
 dynamodb = boto3.resource('dynamodb')
 flight_queries_table = dynamodb.Table('flight_queries')
 ses_client = boto3.client('ses')
@@ -19,7 +16,12 @@ dls = Dl_Searcher()
 
 
 def handler(event, context):
-    for q in fetch_all_queries_from_dynamo(flight_queries_table, LIMIT, MIN_RUN_GAP):
+    # EventBridge puts payload JSON in `detail` field.
+    d = event.get('detail', {})
+    limit = d.get("limit", 100)
+    min_run_gap = d.get("min_run_gap", 3600)
+
+    for q in fetch_all_queries_from_dynamo(flight_queries_table, limit, min_run_gap):
         for air_bound in find_air_bounds(aas, acs, dls, q):
             send_notification(air_bound, q, ses_client)
         update_last_run_time(flight_queries_table, q)
