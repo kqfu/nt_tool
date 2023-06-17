77851650006271a87af131eb555efcfa072e7723
Kevin Fu <qiangkfu@gmail.com>
2023-06-16 22:41:51 -0700

    Integrate Sendgrid

--- cron/main.py
+++ cron/main.py
@@ -1,9 +1,12 @@
 from concurrent.futures import ThreadPoolExecutor
+import json
 import logging
 import time
 
 import functions_framework
-from google.cloud import datastore
+from google.auth import default
+from google.cloud import datastore, secretmanager
+import sendgrid
 
 from flight_query import FlightQuery
 from process import find_air_bounds
@@ -12,12 +15,19 @@ from ac_searcher2 import Ac_Searcher2
 from dl_searcher import Dl_Searcher
 from nt_models import AirBound
 
-FLIGHT_QUERIES_KIND = "flight_queries"
-
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+FLIGHT_QUERIES_KIND = "flight_queries"
+SENDGRID_API_KEY_SECRET_NAME = "sendgrid-api-key"
+
 ds_client = datastore.Client()
+sm_client = secretmanager.SecretManagerServiceClient()
+
+access_secret_response = sm_client.access_secret_version(
+    name=f"projects/{default()[1]}/secrets/{SENDGRID_API_KEY_SECRET_NAME}/versions/latest")
+sendgrid_api_key = access_secret_response.payload.data.decode()
+sg = sendgrid.SendGridAPIClient(sendgrid_api_key)
 
 aas = Aa_Searcher()
 acs = Ac_Searcher2()
@@ -58,7 +68,17 @@ def fetch_all_queries_from_datastore(limit, min_run_gap) \
 
 
 def send_notification(air_bound: AirBound, q: FlightQuery):
-    logger.info("Send notification with %s", q.short_string())
+    logger.info("Sending notification with %s", q.short_string())
+    target_emails = q.email if isinstance(q.email, list) else [q.email]
+    source_email = json.loads(sg.client.verified_senders.get().body)["results"][0]["from_email"]
+
+    message = sendgrid.Mail(
+        from_email=source_email,
+        to_emails=target_emails,
+        subject=f'Reward Ticket Found for {q.origin}-{q.destination} on {q.date}',
+        plain_text_content="\n".join([str(x) for x in air_bound.to_flatted_list()])
+    )
+    sg.send(message)
 
 
 @functions_framework.cloud_event
